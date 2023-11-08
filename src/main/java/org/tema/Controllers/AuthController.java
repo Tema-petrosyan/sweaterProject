95790b8b738a5cf4d920b25ef0492712dbaa70ca
Артём Петросян <tema.petrosyan@gmail.com>
2023-11-08 10:27:51 +0300

    auth preparations 4

--- src/main/java/org/tema/Controllers/AuthController.java
+++ src/main/java/org/tema/Controllers/AuthController.java
@@ -4,12 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.tema.Repositories.UserRepository;
+import org.tema.domain.User;
 import org.tema.domain.dto.UserDTO;
 import org.tema.jwt.JwtCore;
 
@@ -42,11 +47,29 @@ public class AuthController {
         if(userRepository.existsByUsername(userDTO.getUsername())){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("This username already in use!");
         }
+
+        String psw = passwordEncoder.encode(userDTO.getPassword());
+        User user = new User();
+        user.setUsername(userDTO.getUsername());
+        user.setPassword(userDTO.getPassword());
+        userRepository.save(user);
+
+        return ResponseEntity.ok("Success registration");
     }
 
     @PostMapping("/signup")
-    ResponseEntity<?> signin(){
+    ResponseEntity<?> signin(@RequestBody UserDTO userDTO){
+        Authentication authentication = null;
+        try{
+            authentication = manager.authenticate(new UsernamePasswordAuthenticationToken(userDTO.getUsername(), userDTO.getPassword()));
+        }
+        catch (BadCredentialsException e){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
 
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtCore.generateToken(authentication);
+        return ResponseEntity.ok(token);
     }
 
 }
